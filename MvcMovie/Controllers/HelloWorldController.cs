d45f8cf1dcfe6911701f05db0b7ea7a29fe616ea
Lawe Zangena <lawza02@gmail.com>
2021-12-12 23:01:38 +0100

    added a view

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -5,23 +5,17 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // 
-        // GET: /HelloWorld/
-
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
-        // GET: /HelloWorld/Welcome/ 
-        // Requires using System.Text.Encodings.Web;
-        public string Welcome(string name, int ID = 1)
-        { 
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
-        }   
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
 
-        
-
-         
+            return View();
+        }
     }
 }
